b77168ac4694f36865f803fb0d166142b486a0c9
NatHumeniuk <136171176+NatHumeniuk@users.noreply.github.com>
2025-12-11 23:47:35 +0200

    Update API base URL for axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'https://project-node-wt-team4.onrender.com/api';
+axios.defaults.baseURL = 'https://water-tracker-backend-z2wb.onrender.com/api';
 
 export const setAuthHeader = (token) => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
