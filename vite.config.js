124df8dacb8278560e871042307500d302d49ec3
Nat Humeniuk <nat.gumenuk@gmail.com>
2025-12-12 16:02:57 +0200

    upd

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-      plugins: [react(), svgr()],
-      build: { sourcemap: true },
-      resolve: {
-        alias: {
-          src: '/src',
-          components: '/src/components',
-          img: '/src/img',
-          pages: '/src/pages',
-          // redux: '/src/redux',
-          services: "/src/services"
-        },
-      },
-    });
+  base: '/NatHumeniuk/water-tracker-copy/',
+  plugins: [react(), svgr()],
+  build: { sourcemap: true },
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+      img: '/src/img',
+      pages: '/src/pages',
+      // redux: '/src/redux',
+      services: '/src/services',
+    },
+  },
+});
