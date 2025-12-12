756c26f9841b110b9ba9b740f7728c52adb07465
Nat Humeniuk <nat.gumenuk@gmail.com>
2025-12-12 16:39:00 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  base: '/NatHumeniuk/water-tracker-copy/',
+  base: '/water-tracker-copy/',
   plugins: [react(), svgr()],
   build: { sourcemap: true },
   resolve: {
